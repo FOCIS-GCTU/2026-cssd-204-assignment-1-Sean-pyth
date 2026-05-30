7d4a29ced88d4b64bbd8bc76165c8affdd17e06a
Sean-pyth <seancomesback@gmail.com>
2026-05-30 01:19:29 +0000

    Add files via upload
    initials assignment submitted

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Sean Michael Sabah
+# STUDENT ID: 2425402891
+# Email: seancomesback@gmail.com
+# Grader: Augustus
+#
+# On my honor, Sean Michael Sabah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+# Prints my initials in large stylized letters
 
-
+    print()
+    print("...SMS")
+    print()
+    print(".....SSSSSSSSSS........MMMM....MMMM..........SSSSSSSSSS.....")
+    print("....SSSSSSSSSSS........MMMM....MMMM.........SSSSSSSSSSS.....")
+    print("...SSSS................MM.MM..MM.MM........SSSS.............")
+    print("...SSSS................MM.MM..MM.MM........SSSS.............")
+    print(".....SSSSSS............MM.MM..MM.MM..........SSSSSS.........")
+    print(".......SSSSSS..........MM..MMMM..MM............SSSSSS.......")
+    print("...........SSSS........MM..MMMM..MM................SSSS.....")
+    print("...........SSSS........MM...MM...MM................SSSS.....")
+    print("...SSSSSSSSSSS....**...MM...MM...MM...**...SSSSSSSSSSS....**")
+    print("...SSSSSSSSSS.....**...MM...MM...MM...**...SSSSSSSSSS.....**")
+    print()
 
 main()
